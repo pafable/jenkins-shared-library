99ed4def31c99cedd2ea9bb5bda40e5ea78d57a6
pafable <philafable@gmail.com>
2022-06-12 20:59:15 -0400

    test env.WAGGINGTAIL

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -4,7 +4,7 @@ def call(final Closure body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
 
     final String x = System.getProperty("user.dir")
-    println "xx"
+    println xx
     chuckNorris()
     pipeline {
         agent any
