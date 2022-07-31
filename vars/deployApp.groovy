d9b3e0100d56835195c10ca91444aed2dac790b7
phil <philafable@gmail.com>
2022-07-31 11:20:54 -0400

    update

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -5,7 +5,7 @@ def call(final Closure body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
 
     final String x = System.getProperty("user.dir")
-
+    env.setProperty('fizz', 'buzz')
     pipeline {
         agent any
 
@@ -30,7 +30,7 @@ def call(final Closure body) {
                     println "This is x: ${x}"
                     println "This is pwd(): ${pwd()}"
                     println "This is workspace: ${WORKSPACE}"
-                    println "this is foo: \${foo}"
+                    println "this is foo: ${fizz}"
 
                     sh """
                         echo 'SAMPLE KEY PAIRS'
